240db15e2c2838115aac2b81fde187cc0484ff20
YogiDhingani27 <94128664+YogiDhingani27@users.noreply.github.com>
2024-08-14 15:58:36 +0530

    fix: Update Button.stories.ts

--- storybook/src/stories/Button.stories.ts
+++ storybook/src/stories/Button.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Btn } from "@repo/ss-button";
+import { Tooltip as Btn } from "@repo/ss-button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -20,14 +20,3 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
-  args: {
-    label: "Primary Btn",
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: "Secondary Btn",
-  },
-};
