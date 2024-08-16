9348bd5c654c0415e152b4029ea3074bb3accad6
devPragnenkumar <--global>
2024-08-16 10:52:39 +0530

    Storybook cache issue is resolved

---
+++ package/ss-link/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    build: {
+        lib: {
+            entry: resolve(__dirname, "src/index.ts"),
+            fileName: "ss-link",
+            name: "SS-Link",
+        },
+    },
+});
