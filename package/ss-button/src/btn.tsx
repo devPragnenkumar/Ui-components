5363149f09c13a16b2acafe366f312e5ac65b881
Pragnenkumar Amadavadi <pragnenkumar.amadavadi@smartsensesolutions.com>
2024-08-30 14:42:57 +0530

    workflow updated and ssBtn updated

--- package/ss-button/src/btn.tsx
+++ package/ss-button/src/btn.tsx
@@ -2,7 +2,7 @@ function Btn({ label }: Readonly<{ label: string }>) {
   return (
     <button
       onClick={() => {
-        window.alert("SS button clicked updated");
+        window.alert("SS button clicked");
       }}
     >
       {label}
